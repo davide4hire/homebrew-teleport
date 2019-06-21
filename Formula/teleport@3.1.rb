917fa217b1a3049a9ac4b33447f21ef595968509
J$ <jon.monette@gmail.com>
2019-06-20 23:47:18 -0500

    Add 3.0, 3.1, and 3.2 of Teleport

---
+++ Formula/teleport@3.1.rb
@@ -0,0 +1,46 @@
+class TeleportAT32 < Formula
+  desc "Modern SSH server for teams managing distributed infrastructure"
+  homepage "https://gravitational.com/teleport"
+  url "https://github.com/gravitational/teleport/archive/v3.1.9.tar.gz"
+  sha256 "f0b5c58d508a7714dce0e09ef7a1fd957723a318c89a26ce7869daf8169b3e33"
+
+  keg_only :versioned_formula
+
+  depends_on "go" => :build
+
+  conflicts_with "etsh", :because => "both install `tsh` binaries"
+
+  def install
+    ENV["GOOS"] = "darwin"
+    ENV["GOARCH"] = Hardware::CPU.is_64_bit? ? "amd64" : "386"
+    ENV["GOPATH"] = buildpath
+    ENV["GOROOT"] = Formula["go"].opt_libexec
+
+    (buildpath/"src/github.com/gravitational/teleport").install buildpath.children
+    cd "src/github.com/gravitational/teleport" do
+      ENV.deparallelize { system "make", "full" }
+      bin.install Dir["build/*"]
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/teleport version")
+    (testpath/"config.yml").write shell_output("#{bin}/teleport configure")
+      .gsub("0.0.0.0", "127.0.0.1")
+      .gsub("/usr/bin/hostname", "/bin/hostname")
+      .gsub("/var/lib/teleport", testpath)
+      .gsub("/var/run", testpath)
+      .gsub(/https_(.*)/, "")
+    begin
+      pid = spawn("#{bin}/teleport start -c #{testpath}/config.yml")
+      sleep 5
+      system "/usr/bin/curl", "--insecure", "https://localhost:3080"
+      system "/usr/bin/nc", "-z", "localhost", "3022"
+      system "/usr/bin/nc", "-z", "localhost", "3023"
+      system "/usr/bin/nc", "-z", "localhost", "3025"
+    ensure
+      Process.kill(9, pid)
+    end
+  end
+end
--- README.md
+++ README.md
@@ -22,5 +22,8 @@ brew link --force --dry-run teleport@<version>
 ## Versions
 
 - [Teleport 2.7](https://gravitational.com/teleport/docs/ver/2.7/)
+- [Teleport 3.0](https://gravitational.com/teleport/docs/ver/3.0)
+- [Teleport 3.1](https://gravitational.com/teleport/docs/ver/3.1)
+- [Teleport 3.2](https://gravitational.com/teleport/docs/ver/3.2)
 
 _**Note:** Teleport 4.0 is the current stable version installed by default through Homebrew.
