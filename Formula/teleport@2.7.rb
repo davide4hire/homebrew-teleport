be15a0632e4433ba3f96665b9c320c58e4116d38
J$ <jon.monette@gmail.com>
2019-06-20 23:29:27 -0500

    Add teleport 2.7

---
+++ Formula/teleport@2.7.rb
@@ -0,0 +1,46 @@
+class Teleport27 < Formula
+  desc "Modern SSH server for teams managing distributed infrastructure"
+  homepage "https://gravitational.com/teleport"
+  url "https://github.com/gravitational/teleport/archive/v2.7.6.tar.gz"
+  sha256 "fdaefe09fe6831b58ec4fee7875d5de417f04f67fe21be25985067f17991177c"
+
+  keg_only :versioned_formula
+
+  depends_on "go" => :build
+
+  conflicts_with "etsh", :because => "both install `tsh` binaries"
+
+  def install
+    ENV["GOOS"] = "darwin"
+    ENV["GOARCH"] = Hardware::CPU.is_64_bit? ? "amd64" : "386"
+    ENV["GOPATH"] = buildpath
+    ENV["GOROOT"] = Formula["go"].opt_libexec
+
+    (buildpath/"src/github.com/gravitational/teleport").install buildpath.children
+    cd "src/github.com/gravitational/teleport" do
+      ENV.deparallelize { system "make", "full" }
+      bin.install Dir["build/*"]
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/teleport version")
+    (testpath/"config.yml").write shell_output("#{bin}/teleport configure")
+      .gsub("0.0.0.0", "127.0.0.1")
+      .gsub("/usr/bin/hostname", "/bin/hostname")
+      .gsub("/var/lib/teleport", testpath)
+      .gsub("/var/run", testpath)
+      .gsub(/https_(.*)/, "")
+    begin
+      pid = spawn("#{bin}/teleport start -c #{testpath}/config.yml")
+      sleep 5
+      system "/usr/bin/curl", "--insecure", "https://localhost:3080"
+      system "/usr/bin/nc", "-z", "localhost", "3022"
+      system "/usr/bin/nc", "-z", "localhost", "3023"
+      system "/usr/bin/nc", "-z", "localhost", "3025"
+    ensure
+      Process.kill(9, pid)
+    end
+  end
+end
